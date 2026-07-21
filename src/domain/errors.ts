29a39096a366d6ec0ed6c159b501f458a5ed4f44
BrizolaDev <brizoladev@gmail.com>
2026-07-20 23:12:56 -0300

    chore: initial scaffold from hermes-harness

---
+++ src/domain/errors.ts
@@ -0,0 +1,28 @@
+export class AppError extends Error {
+  constructor(
+    message: string,
+    public readonly statusCode: number = 500,
+    public readonly code: string = 'INTERNAL_ERROR',
+  ) {
+    super(message)
+    this.name = this.constructor.name
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(resource: string = 'Resource') {
+    super(`${resource} not found`, 404, 'NOT_FOUND')
+  }
+}
+
+export class ValidationError extends AppError {
+  constructor(message: string) {
+    super(message, 400, 'VALIDATION_ERROR')
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message: string = 'Unauthorized') {
+    super(message, 401, 'UNAUTHORIZED')
+  }
+}
